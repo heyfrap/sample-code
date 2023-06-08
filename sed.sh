9abb87f647f95253f2282821522898da06968eef
naoki.iida <appbrew.labs@gmail.com>
2023-06-08 09:39:31 +0900

    add

--- sed.sh
+++ sed.sh
@@ -1,4 +1,6 @@
 find ./ -name "*.rb" -print | xargs sed -i '' -E 's/its\(\:(.*)\)\ \{ is_expected.to eq (.*) \}/it \{ is_expected.to have_attributes(\1: \2) \}/g'
 find ./ -name "*.rb" -print | xargs sed -i '' -E 's/its\(\:(.*)\)\ \{ is_expected.to eq (.*)\.\.(.*) \}/it \{ is_expected.to have_attributes(\1: \2..\3) \}/g'
 
+sed -i '' -E 's/its\(\:(.*)\)\ \{is_expected.to eq (.*)\.\.(.*) \}/it \{ is_expected.to have_attributes(\1: \2..\3) \}/g' target_file.rb
+
 
