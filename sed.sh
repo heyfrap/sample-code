79be921417fa68ddfc0e5d3c2ca7b8c94715ed31
naoki.iida <appbrew.labs@gmail.com>
2023-06-09 13:02:35 +0900

    add

--- sed.sh
+++ sed.sh
@@ -21,3 +21,5 @@ find ./ -name "*.rb" -print | xargs sed -i '' -E 's/its\(\:(.*)\)[[:space:]]*\{[
 
 find ./ -name "*.rb" -print | xargs sed -i '' -E 's/its\(\:(.*)\)[[:space:]]*\{[[:space:]]*is_expected.to[[:space:]]*eq[[:space:]]*([0-9]+\s*(\+\s*[0-9]+)*)[[:space:]]*\}/it \{ is_expected.to have_attributes(\1: \2) \}/g'
 
+find ./ -name "*.rb" -print | xargs sed -i '' -E 's/its\(\:(.*)\)[[:space:]]*\{[[:space:]]*is_expected.to[[:space:]]*eq[[:space:]]*(.*)\}/it \{ is_expected.to have_attributes(\1: \2) \}/g'
+
