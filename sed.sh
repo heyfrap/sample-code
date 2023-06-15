7492780ab03932842eaf36df70c060f08d32011b
naoki.iida <appbrew.labs@gmail.com>
2023-06-15 16:58:00 +0900

    add

--- sed.sh
+++ sed.sh
@@ -1,1 +1,1 @@
-find /path/to/your/directory -name '*.rb' -exec sed -i '' -E '/to_s\.to_d/!s/to_d/to_s.to_d/g' {} \;
+find /path/to/your/directory -name '*.rb' -exec sed -E -i '' '/to_s\.to_d/!s/\bto_d\b/to_s.to_d/g' {} \;
